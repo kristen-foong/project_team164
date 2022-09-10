a92095a30fb27ae4719313e9ce49519136caaeea
Kristen Foong <kristen.foong@gmail.com>
2022-09-10 14:28:39 -0700

    Create InsightFacade.spec.js

---
+++ test/InsightFacade.spec.js
@@ -0,0 +1,108 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const chai = require("chai");
+const chai_1 = require("chai");
+const fs = require("fs-extra");
+const chaiAsPromised = require("chai-as-promised");
+const IInsightFacade_1 = require("../src/controller/IInsightFacade");
+const InsightFacade_1 = require("../src/controller/InsightFacade");
+const Util_1 = require("../src/Util");
+const TestUtil_1 = require("./TestUtil");
+chai.use(chaiAsPromised);
+describe("InsightFacade Add/Remove/List Dataset", function () {
+    const datasetsToLoad = {
+        onecourse: "./test/data/onecourse.zip",
+        courses: "./test/data/courses.zip",
+        courses1: "./test/data/courses1.zip",
+        empty: "./test/data/courses_empty.zip",
+        wrong: "./test/data/not_a_zip.txt",
+        emptyjson: "./test/data/empty_json.zip",
+        nojson: "./test/data/no_json.zip",
+        norank: "./test/data/no_rank.zip",
+        noresult: "./test/data/no_result.zip",
+        wrongboth: "./test/data/wrong_both.zip",
+        wrong_rank: "./test/data/wrong_rank.zip",
+        wrong_result: "./test/data/wrong_result.zip",
+        rooms: "./test/data/rooms.zip"
+    };
+    let datasets = {};
+    let insightFacade;
+    const cacheDir = __dirname + "/../data";
+    before(function () {
+        Util_1.default.test(`Before all`);
+        if (!fs.existsSync(cacheDir)) {
+            fs.mkdirSync(cacheDir);
+        }
+        for (const id of Object.keys(datasetsToLoad)) {
+            datasets[id] = fs.readFileSync(datasetsToLoad[id]).toString("base64");
+        }
+        try {
+            insightFacade = new InsightFacade_1.default();
+        }
+        catch (err) {
+            Util_1.default.error(err);
+        }
+    });
+    beforeEach(function () {
+        Util_1.default.test(`BeforeTest: ${this.currentTest.title}`);
+    });
+    after(function () {
+        Util_1.default.test(`After: ${this.test.parent.title}`);
+    });
+    afterEach(function () {
+        Util_1.default.test(`AfterTest: ${this.currentTest.title}`);
+        try {
+            fs.removeSync(cacheDir);
+            fs.mkdirSync(cacheDir);
+            insightFacade = new InsightFacade_1.default();
+        }
+        catch (err) {
+            Util_1.default.error(err);
+        }
+    });
+});
+describe("InsightFacade PerformQuery", () => {
+    const datasetsToQuery = {
+        courses: { path: "./test/data/courses.zip", kind: IInsightFacade_1.InsightDatasetKind.Courses },
+        rooms: { path: "./test/data/rooms.zip", kind: IInsightFacade_1.InsightDatasetKind.Rooms }
+    };
+    let insightFacade;
+    let testQueries = [];
+    before(function () {
+        Util_1.default.test(`Before: ${this.test.parent.title}`);
+        try {
+            testQueries = TestUtil_1.default.readTestQueries();
+        }
+        catch (err) {
+            chai_1.expect.fail("", "", `Failed to read one or more test queries. ${err}`);
+        }
+        const loadDatasetPromises = [];
+        insightFacade = new InsightFacade_1.default();
+        for (const id of Object.keys(datasetsToQuery)) {
+            const ds = datasetsToQuery[id];
+            const data = fs.readFileSync(ds.path).toString("base64");
+            loadDatasetPromises.push(insightFacade.addDataset(id, data, ds.kind));
+        }
+        return Promise.all(loadDatasetPromises);
+    });
+    beforeEach(function () {
+        Util_1.default.test(`BeforeTest: ${this.currentTest.title}`);
+    });
+    after(function () {
+        Util_1.default.test(`After: ${this.test.parent.title}`);
+    });
+    afterEach(function () {
+        Util_1.default.test(`AfterTest: ${this.currentTest.title}`);
+    });
+    it("Should run test queries", function () {
+        describe("Dynamic InsightFacade PerformQuery tests", function () {
+            for (const test of testQueries) {
+                it(`[${test.filename}] ${test.title}`, function () {
+                    const futureResult = insightFacade.performQuery(test.query);
+                    return TestUtil_1.default.verifyQueryResult(futureResult, test);
+                });
+            }
+        });
+    });
+});
+//# sourceMappingURL=InsightFacade.spec.js.map
